01958c17f6c8b567ff3f5718ac1584f3c71c4c32
Rob Hogan <rh389@cam.ac.uk>
2013-06-13 15:44:04 +0000

    Add a Minerva event subscriber and send a BookingConfirmation email on receipt of the POST_CONFIRM_BOOKING event

---
+++ EventListener/MinervaSubscriber.php
@@ -0,0 +1,69 @@
+<?php
+namespace Ice\MailerClientBundle\EventListener;
+
+use Ice\MinervaBundle\Event\BookingEvent;
+use Ice\MinervaBundle\Event\MinervaEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Ice\MailerClientBundle\Service\MailerClient;
+
+class MinervaSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var MailerClient
+     */
+    private $mailerClient;
+
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * The array keys are event names and the value can be:
+     *
+     *  * The method name to call (priority defaults to 0)
+     *  * An array composed of the method name to call and the priority
+     *  * An array of arrays composed of the method names to call and respective
+     *    priorities, or 0 if unset
+     *
+     * For instance:
+     *
+     *  * array('eventName' => 'methodName')
+     *  * array('eventName' => array('methodName', $priority))
+     *  * array('eventName' => array(array('methodName1', $priority), array('methodName2'))
+     *
+     * @return array The event names to listen to
+     *
+     * @api
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            MinervaEvents::POST_CONFIRM_BOOKING => 'postConfirmBooking'
+        ];
+    }
+
+    /**
+     * @param BookingEvent $event
+     */
+    public function postConfirmBooking(BookingEvent $event)
+    {
+        $recipient = $event->getBooking()->getAcademicInformation()->getUsername();
+        $this->getMailerClient()->postMail($recipient, 'BookingConfirmation');
+    }
+
+    /**
+     * @param \Ice\MailerClientBundle\Service\MailerClient $mailerClient
+     * @return MinervaSubscriber
+     */
+    public function setMailerClient($mailerClient)
+    {
+        $this->mailerClient = $mailerClient;
+        return $this;
+    }
+
+    /**
+     * @return \Ice\MailerClientBundle\Service\MailerClient
+     */
+    public function getMailerClient()
+    {
+        return $this->mailerClient;
+    }
+}
